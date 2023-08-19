40a67a75432dafb8517e463a00742fa70827dafe
Paul Marcelin Bejan <paulmarcelinbejan.developer@gmail.com>
2023-08-19 16:59:55 +0300

    feat: LocalDate serializer/deserializer

---
+++ toolbox-jackson/src/main/java/com/paulmarcelinbejan/toolbox/jackson/serializer/LocalDateSerializer.java
@@ -0,0 +1,21 @@
+package com.paulmarcelinbejan.toolbox.jackson.serializer;
+
+import java.io.IOException;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.JsonSerializer;
+import com.fasterxml.jackson.databind.SerializerProvider;
+import com.paulmarcelinbejan.toolbox.utils.time.pattern.date.DatePattern;
+
+public class LocalDateSerializer extends JsonSerializer<LocalDate> {
+
+	private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern(DatePattern.Numeric.WithSlashSeparator.ddMMyyyy.getValue());
+	
+	@Override
+	public void serialize(LocalDate value, JsonGenerator gen, SerializerProvider serializers) throws IOException {
+		gen.writeString(value.format(FORMATTER));
+	}
+
+}
