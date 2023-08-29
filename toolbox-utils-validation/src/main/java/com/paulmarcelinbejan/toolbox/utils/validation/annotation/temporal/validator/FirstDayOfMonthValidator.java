484fdf4ede6c41570e0b64b0fc04c375285d14f7
Paul Marcelin Bejan <paulmarcelinbejan.developer@gmail.com>
2023-08-29 23:23:53 +0200

    feat: FirstDayOfMonth validation

---
+++ toolbox-utils-validation/src/main/java/com/paulmarcelinbejan/toolbox/utils/validation/annotation/temporal/validator/FirstDayOfMonthValidator.java
@@ -0,0 +1,21 @@
+package com.paulmarcelinbejan.toolbox.utils.validation.annotation.temporal.validator;
+
+import java.time.LocalDate;
+
+import com.paulmarcelinbejan.toolbox.utils.time.DateUtils;
+import com.paulmarcelinbejan.toolbox.utils.validation.annotation.temporal.FirstDayOfMonth;
+
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+
+public class FirstDayOfMonthValidator implements ConstraintValidator<FirstDayOfMonth, LocalDate> {
+
+    /**
+     * Checks if is valid.
+     */
+    @Override
+    public boolean isValid(LocalDate localDate, ConstraintValidatorContext constraintValidatorContext) {
+    	return DateUtils.isFirstDayOfMonth(localDate);
+    }
+
+}
