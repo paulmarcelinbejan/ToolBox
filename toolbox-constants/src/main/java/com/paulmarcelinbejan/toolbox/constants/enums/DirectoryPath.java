22f84f63a30bfc9529037719d3be2795c4e70a65
Paul Marcelin Bejan <paulmarcelinbejan.developer@gmail.com>
2023-09-17 01:28:09 +0200

    feat: SRC_MAIN_RESOURCES into DirectoryPath enum

--- toolbox-constants/src/main/java/com/paulmarcelinbejan/toolbox/constants/enums/DirectoryPath.java
+++ toolbox-constants/src/main/java/com/paulmarcelinbejan/toolbox/constants/enums/DirectoryPath.java
@@ -2,6 +2,7 @@ package com.paulmarcelinbejan.toolbox.constants.enums;
 
 public enum DirectoryPath {
 
+	SRC_MAIN_RESOURCES("./src/main/resources/"),
 	SRC_TEST_RESOURCES("./src/test/resources/");
 	
     public final String value;
