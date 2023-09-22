1aac4fbc0f586bfdc3b13bb6793f68f0eaa5d53f
Paul Marcelin Bejan <paulmarcelinbejan.developer@gmail.com>
2023-09-22 23:12:00 +0200

    feat: JUnit test JsonFileUtils

---
+++ toolbox-utils-io/src/test/java/com/paulmarcelinbejan/toolbox/utils/io/json/JsonFileUtilsTest.java
@@ -0,0 +1,64 @@
+package com.paulmarcelinbejan.toolbox.utils.io.json;
+
+import static com.paulmarcelinbejan.toolbox.utils.io.config.FileType.JSON;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.fail;
+
+import java.io.IOException;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+
+import com.paulmarcelinbejan.toolbox.constants.enums.DirectoryPath;
+import com.paulmarcelinbejan.toolbox.utils.io.FileUtils;
+import com.paulmarcelinbejan.toolbox.utils.io.config.FileInfo;
+import com.paulmarcelinbejan.toolbox.utils.io.json.dummy.Employee;
+
+class JsonFileUtilsTest {
+
+	@Test
+	void testReadOne() throws IOException {
+		JsonFileUtils<Employee> json = new JsonFileUtils<>(Employee.class);
+		FileInfo fileInfo = new FileInfo(DirectoryPath.SRC_TEST_RESOURCES.value, "employee");
+		Employee employee = json.read(fileInfo);
+		assertNotNull(employee);
+	}
+	
+	@Test
+	void testReadMany() throws IOException {
+		JsonFileUtils<Employee> json = new JsonFileUtils<>(Employee.class);
+		FileInfo fileInfo = new FileInfo(DirectoryPath.SRC_TEST_RESOURCES.value, "employees");
+		List<Employee> employees = json.readList(fileInfo);
+		assertNotNull(employees);
+		assertEquals(3, employees.size());
+	}
+	
+	@Test
+	void testWriteOne() {
+		Employee employee = new Employee("Paul", "P");
+		JsonFileUtils<Employee> json = new JsonFileUtils<>(Employee.class);
+		FileInfo fileInfo = new FileInfo(DirectoryPath.SRC_TEST_RESOURCES.value, "employeeWrittenByJava");
+		try {
+			json.write(fileInfo, employee);
+		} catch (IOException e) {
+			fail();
+		}
+		assertDoesNotThrow(() -> FileUtils.createFileReader(fileInfo, JSON));
+	}
+	
+	@Test
+	void testWriteMany() throws IOException {
+		List<Employee> employees = List.of(new Employee("a", "a"), new Employee("b", "b"), new Employee("c", "c"), new Employee("d", "d"));
+		JsonFileUtils<Employee> json = new JsonFileUtils<>(Employee.class);
+		FileInfo fileInfo = new FileInfo(DirectoryPath.SRC_TEST_RESOURCES.value, "employeesWrittenByJava");
+		try {
+			json.writeList(fileInfo, employees);
+		} catch (IOException e) {
+			fail();
+		}
+		assertDoesNotThrow(() -> FileUtils.createFileReader(fileInfo, JSON));
+	}
+	
+}
---
+++ toolbox-utils-io/src/test/java/com/paulmarcelinbejan/toolbox/utils/io/json/dummy/Employee.java
@@ -0,0 +1,16 @@
+package com.paulmarcelinbejan.toolbox.utils.io.json.dummy;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Employee {
+
+	private String firstName;
+	
+	private String lastName;
+	
+}
