0a72061c85b60a5f1fcf3258f08a8748c180ecf5
Paul Marcelin Bejan <paulmarcelinbejan.developer@gmail.com>
2023-11-22 14:37:17 +0100

    feat: use of lombok

--- toolbox-utils-csv/src/main/java/com/paulmarcelinbejan/toolbox/utils/csv/config/CsvFileUtilsConfig.java
+++ toolbox-utils-csv/src/main/java/com/paulmarcelinbejan/toolbox/utils/csv/config/CsvFileUtilsConfig.java
@@ -1,22 +1,15 @@
 package com.paulmarcelinbejan.toolbox.utils.csv.config;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
+@Getter
+@AllArgsConstructor
 public class CsvFileUtilsConfig {
-
-	public CsvFileUtilsConfig(
-			CsvReaderConfig readerConfig, 
-			CsvWriterConfig writerConfig) {
-		this.readerConfig = readerConfig;
-		this.writerConfig = writerConfig;
-	}
 	
 	public static final CsvFileUtilsConfig DEFAULT = new CsvFileUtilsConfig(CsvReaderConfig.DEFAULT, CsvWriterConfig.DEFAULT);
 	
-	@Getter
 	private final CsvReaderConfig readerConfig;
-	
-	@Getter
 	private final CsvWriterConfig writerConfig;
 	
 }
