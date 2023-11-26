42449e2ecf41f52ea30b488f66970f93ee4f2c92
Paul Marcelin Bejan <paulmarcelinbejan.developer@gmail.com>
2023-11-26 16:40:01 +0100

    fix: removed template from toolbox-web (moved to toolbox-service-helper)

--- toolbox-web/ToolBox-Web-Template/TEMPLATES/TemplateService.java
+++
@@ -1,8 +0,0 @@
-package ${PACKAGE}.${ENTITY_LOWERCASE}.service;
-
-import ${PACKAGE}.${ENTITY_LOWERCASE}.entity.${ENTITY};
-import com.paulmarcelinbejan.toolbox.web.service.aggregate.CrudService;
-
-public interface ${ENTITY}Service extends CrudService<${ID_TYPE}, ${ENTITY}> {
-
-}
