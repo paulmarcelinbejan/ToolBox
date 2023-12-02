7b70f5a77258f5d4eb9a7caa6d10aea32caadaf4
Paul Marcelin Bejan <paulmarcelinbejan.developer@gmail.com>
2023-12-02 17:19:54 +0100

    javadoc: EnumBase and DateTimePatternBase

--- toolbox-base/src/main/java/io/github/paulmarcelinbejan/toolbox/base/enums/EnumBase.java
+++ toolbox-base/src/main/java/io/github/paulmarcelinbejan/toolbox/base/enums/EnumBase.java
@@ -1,7 +1,16 @@
 package io.github.paulmarcelinbejan.toolbox.base.enums;
 
+/**
+ * A base interface for enums, providing a method to obtain the enum's value.
+ */
 public interface EnumBase {
 
-	public String getValue();
-	
+    /**
+     * Gets the value associated with the enum.
+     *
+     * @return The value of the enum.
+     */
+    String getValue();
+    
 }
+
--- toolbox-utils-time/src/main/java/io/github/paulmarcelinbejan/toolbox/utils/time/pattern/DateTimePatternBase.java
+++ toolbox-utils-time/src/main/java/io/github/paulmarcelinbejan/toolbox/utils/time/pattern/DateTimePatternBase.java
@@ -4,8 +4,18 @@ import java.time.format.DateTimeFormatter;
 
 import io.github.paulmarcelinbejan.toolbox.base.enums.EnumBase;
 
+/**
+ * A base interface for date and time pattern enums, extending {@link EnumBase}.
+ * It provides a method to obtain the associated {@link DateTimeFormatter}.
+ */
 public interface DateTimePatternBase extends EnumBase {
-	
-	public DateTimeFormatter getFormatter();
-	
+
+    /**
+     * Gets the {@link DateTimeFormatter} associated with this date and time pattern.
+     *
+     * @return The {@link DateTimeFormatter} for this date and time pattern.
+     */
+    DateTimeFormatter getFormatter();
+    
 }
+
