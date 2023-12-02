bf5f44e95c9133653f5118ab50af07a17ac6058a
Paul Marcelin Bejan <paulmarcelinbejan.developer@gmail.com>
2023-12-02 17:16:28 +0100

    javadoc: LocalDateTimeFormatUtils

--- toolbox-utils-time/src/main/java/io/github/paulmarcelinbejan/toolbox/utils/time/format/LocalDateTimeFormatUtils.java
+++ toolbox-utils-time/src/main/java/io/github/paulmarcelinbejan/toolbox/utils/time/format/LocalDateTimeFormatUtils.java
@@ -21,87 +21,160 @@ import lombok.NoArgsConstructor;
 public class LocalDateTimeFormatUtils {
 	
 	/**
-	 * Build a pattern combining DatePattern with TimePattern.
-	 * 
-	 * @param DatePattern datePattern
-	 * @param String dateTimeConcatenation, 'T' as in ISO format, ' ' or whatever
-	 * @param TimePattern timePattern
-	 * 
-	 * @return a new DateTimePattern
+	 * Build a pattern by combining DatePattern with TimePattern.
+	 *
+	 * @param <DatePattern> The type of DatePattern, extending DatePatternBase.
+	 * @param <TimePattern> The type of TimePattern, extending TimePatternBase.
+	 * @param datePattern   The DatePattern to be used.
+	 * @param dateTimeConcatenation The String representing the concatenation between date and time, such as 'T' in ISO format or ' '.
+	 * @param timePattern   The TimePattern to be used.
+	 * @return A new DateTimePattern as a String.
 	 */
 	public static final <DatePattern extends DatePatternBase, TimePattern extends TimePatternBase> String buildPattern(DatePattern datePattern, String dateTimeConcatenation, TimePattern timePattern) {
-		return new StringBuilder()
-				.append(datePattern.getValue())
-				.append(dateTimeConcatenation)
-				.append(timePattern.getValue())
-				.toString();
+	    return new StringBuilder()
+	            .append(datePattern.getValue())
+	            .append(dateTimeConcatenation)
+	            .append(timePattern.getValue())
+	            .toString();
 	}
-	
-	public static final <DatePattern extends DatePatternBase, TimePattern extends TimePatternBase> String toString(LocalDateTime localDateTime, DatePattern datePattern, String dateTimeConcatenation, TimePattern timePattern) {
-		return formatter(buildPattern(datePattern, dateTimeConcatenation, timePattern)).format(localDateTime);
-	}
-	
-	public static final <DatePattern extends DatePatternBase, TimePattern extends TimePatternBase> String toString(LocalDateTime localDateTime, DatePattern datePattern, String dateTimeConcatenation, TimePattern timePattern, Locale locale) {
-		return formatterLocalized(buildPattern(datePattern, dateTimeConcatenation, timePattern), locale).format(localDateTime);
-	}
-	
+
 	/**
-	 * @throws IllegalArgumentException if the pattern is invalid
+	 * Converts a LocalDateTime to a formatted String using the specified DatePattern, dateTimeConcatenation, and TimePattern.
+	 *
+	 * @param <DatePattern> The type of DatePattern, extending DatePatternBase.
+	 * @param <TimePattern> The type of TimePattern, extending TimePatternBase.
+	 * @param localDateTime The LocalDateTime to be formatted.
+	 * @param datePattern   The DatePattern to be used.
+	 * @param dateTimeConcatenation The String representing the concatenation between date and time, such as 'T' in ISO format or ' '.
+	 * @param timePattern   The TimePattern to be used.
+	 * @return A formatted String representation of the LocalDateTime.
+	 */
+	public static final <DatePattern extends DatePatternBase, TimePattern extends TimePatternBase> String toString(LocalDateTime localDateTime, DatePattern datePattern, String dateTimeConcatenation, TimePattern timePattern) {
+	    return formatter(buildPattern(datePattern, dateTimeConcatenation, timePattern)).format(localDateTime);
+	}
+
+	/**
+	 * Converts a LocalDateTime to a formatted String using the specified DatePattern, dateTimeConcatenation, TimePattern, and Locale.
+	 *
+	 * @param <DatePattern> The type of DatePattern, extending DatePatternBase.
+	 * @param <TimePattern> The type of TimePattern, extending TimePatternBase.
+	 * @param localDateTime The LocalDateTime to be formatted.
+	 * @param datePattern   The DatePattern to be used.
+	 * @param dateTimeConcatenation The String representing the concatenation between date and time, such as 'T' in ISO format or ' '.
+	 * @param timePattern   The TimePattern to be used.
+	 * @param locale        The Locale to be applied.
+	 * @return A formatted String representation of the LocalDateTime with the specified Locale.
+	 */
+	public static final <DatePattern extends DatePatternBase, TimePattern extends TimePatternBase> String toString(LocalDateTime localDateTime, DatePattern datePattern, String dateTimeConcatenation, TimePattern timePattern, Locale locale) {
+	    return formatterLocalized(buildPattern(datePattern, dateTimeConcatenation, timePattern), locale).format(localDateTime);
+	}
+
+	/**
+	 * Converts a LocalDateTime to a formatted String using the specified DateTimePattern.
+	 *
+	 * @param <DateTimePattern> The type of DateTimePattern, extending DateTimePatternBase.
+	 * @param localDateTime The LocalDateTime to be formatted.
+	 * @param dateTimePattern The DateTimePattern to be used.
+	 * @return A formatted String representation of the LocalDateTime.
+	 * @throws IllegalArgumentException if the pattern is invalid.
 	 */
 	public static final <DateTimePattern extends DateTimePatternBase> String toString(LocalDateTime localDateTime, DateTimePattern dateTimePattern) {
-		return dateTimePattern.getFormatter().format(localDateTime);
+	    return dateTimePattern.getFormatter().format(localDateTime);
 	}
-	
+
 	/**
-	 * @throws IllegalArgumentException if the pattern is invalid
+	 * Converts a LocalDateTime to a formatted String using the specified DateTimePattern and Locale.
+	 *
+	 * @param <DateTimePattern> The type of DateTimePattern, extending DateTimePatternBase.
+	 * @param localDateTime The LocalDateTime to be formatted.
+	 * @param dateTimePattern The DateTimePattern to be used.
+	 * @param locale        The Locale to be applied.
+	 * @return A formatted String representation of the LocalDateTime with the specified Locale.
+	 * @throws IllegalArgumentException if the pattern is invalid.
 	 */
 	public static final <DateTimePattern extends DateTimePatternBase> String toString(LocalDateTime localDateTime, DateTimePattern dateTimePattern, Locale locale) {
-		return formatterLocalized(dateTimePattern.getFormatter(), locale).format(localDateTime);
+	    return formatterLocalized(dateTimePattern.getFormatter(), locale).format(localDateTime);
 	}
-	
+
 	/**
-	 * @throws IllegalArgumentException if the pattern is invalid
+	 * Converts a LocalDateTime to a formatted String using the specified pattern.
+	 *
+	 * @param localDateTime The LocalDateTime to be formatted.
+	 * @param dateTimePattern The pattern to be used.
+	 * @return A formatted String representation of the LocalDateTime.
+	 * @throws IllegalArgumentException if the pattern is invalid.
 	 */
 	public static final String toString(LocalDateTime localDateTime, String dateTimePattern) {
-		return formatter(dateTimePattern).format(localDateTime);
+	    return formatter(dateTimePattern).format(localDateTime);
 	}
-	
+
 	/**
-	 * @throws IllegalArgumentException if the pattern is invalid
+	 * Converts a LocalDateTime to a formatted String using the specified pattern and Locale.
+	 *
+	 * @param localDateTime The LocalDateTime to be formatted.
+	 * @param dateTimePattern The pattern to be used.
+	 * @param locale        The Locale to be applied.
+	 * @return A formatted String representation of the LocalDateTime with the specified Locale.
+	 * @throws IllegalArgumentException if the pattern is invalid.
 	 */
 	public static final String toString(LocalDateTime localDateTime, String dateTimePattern, Locale locale) {
-		return formatterLocalized(dateTimePattern, locale).format(localDateTime);
+	    return formatterLocalized(dateTimePattern, locale).format(localDateTime);
 	}
-	
+
 	// To LocalDateTime
-	
+
 	/**
-	 * @throws DateTimeParseException - if the text cannot be parsed
+	 * Parses a String to obtain a LocalDateTime using the specified DateTimePattern.
+	 *
+	 * @param <DateTimePattern> The type of DateTimePattern, extending DateTimePatternBase.
+	 * @param dateTime The String to be parsed.
+	 * @param dateTimePattern The DateTimePattern to be used.
+	 * @return A LocalDateTime parsed from the input String.
+	 * @throws DateTimeParseException if the text cannot be parsed.
 	 */
 	public static final <DateTimePattern extends DateTimePatternBase> LocalDateTime toLocalDateTime(String dateTime, DateTimePattern dateTimePattern) {
-		return LocalDateTime.parse(dateTime, dateTimePattern.getFormatter());
+	    return LocalDateTime.parse(dateTime, dateTimePattern.getFormatter());
 	}
-	
+
 	/**
-	 * @throws DateTimeParseException - if the text cannot be parsed
+	 * Parses a String to obtain a LocalDateTime using the specified DateTimePattern and Locale.
+	 *
+	 * @param <DateTimePattern> The type of DateTimePattern, extending DateTimePatternBase.
+	 * @param dateTime The String to be parsed.
+	 * @param dateTimePattern The DateTimePattern to be used.
+	 * @param locale        The Locale to be applied.
+	 * @return A LocalDateTime parsed from the input String with the specified Locale.
+	 * @throws DateTimeParseException if the text cannot be parsed.
 	 */
 	public static final <DateTimePattern extends DateTimePatternBase> LocalDateTime toLocalDateTime(String dateTime, DateTimePattern dateTimePattern, Locale locale) {
-		return LocalDateTime.parse(dateTime, formatterLocalized(dateTimePattern.getFormatter(), locale));
+	    return LocalDateTime.parse(dateTime, formatterLocalized(dateTimePattern.getFormatter(), locale));
 	}
-	
+
 	/**
-	 * @throws IllegalArgumentException if the pattern is invalid
+	 * Parses a String to obtain a LocalDateTime using the specified pattern.
+	 *
+	 * @param date The String to be parsed.
+	 * @param dateTimePattern The pattern to be used.
+	 * @return A LocalDateTime parsed from the input String.
+	 * @throws IllegalArgumentException if the pattern is invalid.
+	 * @throws DateTimeParseException if the text cannot be parsed.
 	 */
 	public static final LocalDateTime toLocalDateTime(String date, String dateTimePattern) {
-		return LocalDateTime.parse(date, formatter(dateTimePattern));
+	    return LocalDateTime.parse(date, formatter(dateTimePattern));
 	}
-	
+
 	/**
-	 * @throws IllegalArgumentException if the pattern is invalid
-	 * @throws DateTimeParseException - if the text cannot be parsed
+	 * Parses a String to obtain a LocalDateTime using the specified pattern and Locale.
+	 *
+	 * @param date The String to be parsed.
+	 * @param dateTimePattern The pattern to be used.
+	 * @param locale        The Locale to be applied.
+	 * @return A LocalDateTime parsed from the input String with the specified Locale.
+	 * @throws IllegalArgumentException if the pattern is invalid.
+	 * @throws DateTimeParseException if the text cannot be parsed.
 	 */
 	public static final LocalDateTime toLocalDateTime(String date, String dateTimePattern, Locale locale) {
-		return LocalDateTime.parse(date, formatterLocalized(dateTimePattern, locale));
+	    return LocalDateTime.parse(date, formatterLocalized(dateTimePattern, locale));
 	}
 	
 }
