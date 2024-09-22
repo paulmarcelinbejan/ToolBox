a270e8b486c33f88e5d928a51833eaa02697fbe0
Paul Marcelin Bejan <paulmarcelinbejan.developer@gmail.com>
2024-09-22 19:55:09 +0200

    feat: javadoc

--- toolbox-configuration/src/main/java/io/github/paulmarcelinbejan/toolbox/configuration/AbstractListBeanDefinitionRegistrar.java
+++ toolbox-configuration/src/main/java/io/github/paulmarcelinbejan/toolbox/configuration/AbstractListBeanDefinitionRegistrar.java
@@ -11,8 +11,25 @@ import org.springframework.boot.context.properties.bind.Bindable;
 import org.springframework.boot.context.properties.bind.Binder;
 import org.springframework.core.env.Environment;
 
+/**
+ * Abstract base class for registering a list of bean definitions from a configuration
+ * class. This class implements {@link BeanDefinitionRegistryPostProcessor} and processes
+ * a list of configurations to register bean definitions in the {@link BeanDefinitionRegistry}.
+ * 
+ * @param <Configuration> The type of configuration objects used to define beans.
+ */
 public abstract class AbstractListBeanDefinitionRegistrar<Configuration> implements BeanDefinitionRegistryPostProcessor {
 
+	/**
+     * Constructor to initialize the {@code AbstractListBeanDefinitionRegistrar} with
+     * the environment, a property name, and the class type of the configuration.
+     * The configurations are bound from the environment using the given property.
+     * 
+     * @param environment the {@link Environment} from which properties are retrieved.
+     * @param property the property name that contains the configuration list.
+     * @param configurationClass the class type of the configuration to be bound.
+     * @throws IllegalStateException if the configurations cannot be bound from the environment.
+     */
 	protected AbstractListBeanDefinitionRegistrar(
 			Environment environment, 
 			String property,
@@ -22,13 +39,31 @@ public abstract class AbstractListBeanDefinitionRegistrar<Configuration> impleme
 				.orElseThrow(IllegalStateException::new);
 	}
 
+	/**
+     * List of configuration objects used to define and register beans.
+     */
 	private final List<Configuration> configurations;
 
+
+	/**
+     * Processes the {@link BeanDefinitionRegistry} to register bean definitions
+     * for each configuration in the list of configurations.
+     * 
+     * @param registry the {@link BeanDefinitionRegistry} to register the beans with.
+     * @throws BeansException if an error occurs during bean definition registration.
+     */
 	@Override
 	public void postProcessBeanDefinitionRegistry(BeanDefinitionRegistry registry) throws BeansException {
 		configurations.forEach(configuration -> registerBeanDefinition(registry, configuration));
 	}
 
+	/**
+     * Registers a single bean definition in the provided {@link BeanDefinitionRegistry}
+     * based on the given configuration object.
+     * 
+     * @param registry the registry where the bean definition should be registered.
+     * @param configuration the configuration object used to create the bean definition.
+     */
 	private void registerBeanDefinition(BeanDefinitionRegistry registry, Configuration configuration) {
 		GenericBeanDefinition beanDefinition = new GenericBeanDefinition();
 		beanDefinition.setBeanClass(getBeanClass());
@@ -37,19 +72,31 @@ public abstract class AbstractListBeanDefinitionRegistrar<Configuration> impleme
 	}
 
 	/**
-	 * @return the Class of the Bean to instance
+	 * Returns the class type of the bean to be instantiated. This method specifies
+	 * the class that will be used as the blueprint for creating the bean instance
+	 * during the registration process. Subclasses must provide the specific class
+	 * that defines the bean's structure and behavior.
+	 * 
+	 * @return the {@code Class} of the bean to be instantiated.
 	 */
 	protected abstract Class<?> getBeanClass();
 	
 	/**
-	 * @param configuration element
-	 * @return a Supplier used to create the instance of the bean
+	 * Returns a {@code Supplier} that is used to create an instance of the bean based on
+	 * the provided configuration object that defines the bean.
+	 * 
+	 * @param configuration the configuration object
+	 * @return a {@code Supplier} that provides the bean instance.
 	 */
 	protected abstract Supplier<?> getBeanInstanceSupplier(Configuration configuration);
 
 	/**
-	 * @param configuration element
-	 * @return the name to apply to the bean
+	 * Returns the name to apply to the bean based on the provided map entry. The key of
+	 * the entry typically represents the desired bean name, and the value contains the
+	 * configuration object.
+	 * 
+	 * @param configuration the configuration object
+	 * @return the name to assign to the bean in the {@link BeanDefinitionRegistry}.
 	 */
 	protected abstract String getBeanName(Configuration configuration);
 
--- toolbox-configuration/src/main/java/io/github/paulmarcelinbejan/toolbox/configuration/AbstractMapBeanDefinitionRegistrar.java
+++ toolbox-configuration/src/main/java/io/github/paulmarcelinbejan/toolbox/configuration/AbstractMapBeanDefinitionRegistrar.java
@@ -12,8 +12,25 @@ import org.springframework.boot.context.properties.bind.Bindable;
 import org.springframework.boot.context.properties.bind.Binder;
 import org.springframework.core.env.Environment;
 
+/**
+ * Abstract base class for registering bean definitions from a map of configurations.
+ * This class implements {@link BeanDefinitionRegistryPostProcessor} and processes
+ * a map of configurations to register bean definitions in the {@link BeanDefinitionRegistry}.
+ * 
+ * @param <Configuration> The type of configuration objects used to define beans.
+ */
 public abstract class AbstractMapBeanDefinitionRegistrar<Configuration> implements BeanDefinitionRegistryPostProcessor {
 
+	/**
+     * Constructor to initialize the {@code AbstractMapBeanDefinitionRegistrar} with
+     * the environment, a property name, and the class type of the configuration.
+     * The configurations are bound from the environment using the given property.
+     * 
+     * @param environment the {@link Environment} from which properties are retrieved.
+     * @param property the property name that contains the map of configurations.
+     * @param configurationClass the class type of the configuration to be bound.
+     * @throws IllegalStateException if the configurations cannot be bound from the environment.
+     */
 	protected AbstractMapBeanDefinitionRegistrar(
 			Environment environment, 
 			String property,
@@ -23,13 +40,32 @@ public abstract class AbstractMapBeanDefinitionRegistrar<Configuration> implemen
 				.orElseThrow(IllegalStateException::new);
 	}
 
+	/**
+     * Map of configuration objects used to define and register beans, with the keys
+     * being the bean names.
+     */
 	private final Map<String, Configuration> configurations;
 
+	/**
+     * Processes the {@link BeanDefinitionRegistry} to register bean definitions
+     * for each entry in the map of configurations.
+     * 
+     * @param registry the {@link BeanDefinitionRegistry} to register the beans with.
+     * @throws BeansException if an error occurs during bean definition registration.
+     */
 	@Override
 	public void postProcessBeanDefinitionRegistry(BeanDefinitionRegistry registry) throws BeansException {
 		configurations.entrySet().forEach(entry -> registerBeanDefinition(registry, entry));
 	}
 
+	/**
+     * Registers a single bean definition in the provided {@link BeanDefinitionRegistry}
+     * based on the given map entry (key-value pair).
+     * 
+     * @param registry the registry where the bean definition should be registered.
+     * @param entry the map entry where the key is the bean name and the value is
+     *              the configuration object used to create the bean definition.
+     */
 	private void registerBeanDefinition(BeanDefinitionRegistry registry, Entry<String, Configuration> entry) {
 		GenericBeanDefinition beanDefinition = new GenericBeanDefinition();
 		beanDefinition.setBeanClass(getBeanClass());
@@ -38,19 +74,32 @@ public abstract class AbstractMapBeanDefinitionRegistrar<Configuration> implemen
 	}
 
 	/**
-	 * @return the Class of the Bean to instance
+	 * Returns the class type of the bean to be instantiated. This method specifies
+	 * the class that will be used as the blueprint for creating the bean instance
+	 * during the registration process. Subclasses must provide the specific class
+	 * that defines the bean's structure and behavior.
+	 * 
+	 * @return the {@code Class} of the bean to be instantiated.
 	 */
 	protected abstract Class<?> getBeanClass();
 	
 	/**
-	 * @param configuration element
-	 * @return a Supplier used to create the instance of the bean
+	 * Returns a {@code Supplier} that is used to create an instance of the bean based on
+	 * the provided map entry. The entry consists of a key (bean name) and a configuration
+	 * object that defines the bean.
+	 * 
+	 * @param entry a map entry containing the bean name and configuration object.
+	 * @return a {@code Supplier} that provides the bean instance.
 	 */
 	protected abstract Supplier<?> getBeanInstanceSupplier(Entry<String, Configuration> entry);
 
 	/**
-	 * @param configuration element
-	 * @return the name to apply to the bean
+	 * Returns the name to apply to the bean based on the provided map entry. The key of
+	 * the entry typically represents the desired bean name, and the value contains the
+	 * configuration object.
+	 * 
+	 * @param entry a map entry containing the bean name and configuration object.
+	 * @return the name to assign to the bean in the {@link BeanDefinitionRegistry}.
 	 */
 	protected abstract String getBeanName(Entry<String, Configuration> entry);
 
