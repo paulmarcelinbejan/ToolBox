016220ac75f1630d21214aebec604d0fbd1e6cbf
Paul Marcelin Bejan <paulmarcelinbejan.developer@gmail.com>
2025-01-26 22:52:28 +0100

    feat: MappingException and ValidationException implements DetailedException

--- toolbox-exception/src/main/java/io/github/paulmarcelinbejan/toolbox/exception/mapping/MappingException.java
+++ toolbox-exception/src/main/java/io/github/paulmarcelinbejan/toolbox/exception/mapping/MappingException.java
@@ -1,19 +1,60 @@
 package io.github.paulmarcelinbejan.toolbox.exception.mapping;
 
-public class MappingException extends RuntimeException {
+import java.util.HashMap;
+import java.util.Map;
 
-	private static final long serialVersionUID = -362407787401934174L;
+import io.github.paulmarcelinbejan.toolbox.exception.aware.DetailedException;
+import lombok.Getter;
 
-	public MappingException(String message) {
-		super(message);
-	}
+@Getter
+public class MappingException extends RuntimeException implements DetailedException {
 
-	public MappingException(String message, Throwable cause) {
-		super(message, cause);
-	}
+	private static final long serialVersionUID = -3655961291465196761L;
+
+	private final String messageCode;
+	
+	private final Map<String, String> messageParams;
 
 	public MappingException(Throwable cause) {
 		super(cause);
+		this.messageCode = "";
+		this.messageParams = new HashMap<>();
+	}
+	
+	public MappingException(String message) {
+		super(message);
+		this.messageCode = "";
+		this.messageParams = new HashMap<>();
+	}
+	
+	public MappingException(String message, Throwable cause) {
+		super(message, cause);
+		this.messageCode = "";
+		this.messageParams = new HashMap<>();
+	}
+	
+	public MappingException(String message, String messageCode) {
+		super(message);
+		this.messageCode = messageCode;
+		this.messageParams = new HashMap<>();
+	}
+	
+	public MappingException(String message, String messageCode, Throwable cause) {
+		super(message, cause);
+		this.messageCode = messageCode;
+		this.messageParams = new HashMap<>();
+	}
+	
+	public MappingException(String message, String messageCode, Map<String, String> messageParams) {
+		super(message);
+		this.messageCode = messageCode;
+		this.messageParams = messageParams;
+	}
+	
+	public MappingException(String message, String messageCode, Map<String, String> messageParams, Throwable cause) {
+		super(message, cause);
+		this.messageCode = messageCode;
+		this.messageParams = messageParams;
 	}
 	
 }
--- toolbox-exception/src/main/java/io/github/paulmarcelinbejan/toolbox/exception/validation/ValidationException.java
+++ toolbox-exception/src/main/java/io/github/paulmarcelinbejan/toolbox/exception/validation/ValidationException.java
@@ -1,19 +1,60 @@
 package io.github.paulmarcelinbejan.toolbox.exception.validation;
 
-public class ValidationException extends RuntimeException {
+import java.util.HashMap;
+import java.util.Map;
+
+import io.github.paulmarcelinbejan.toolbox.exception.aware.DetailedException;
+import lombok.Getter;
+
+@Getter
+public class ValidationException extends RuntimeException implements DetailedException {
 
 	private static final long serialVersionUID = -7392271628640948828L;
 
-	public ValidationException(String message) {
-		super(message);
-	}
-
-	public ValidationException(String message, Throwable cause) {
-		super(message, cause);
-	}
-
+	private final String messageCode;
+	
+	private final Map<String, String> messageParams;
+	
 	public ValidationException(Throwable cause) {
 		super(cause);
+		this.messageCode = "";
+		this.messageParams = new HashMap<>();
+	}
+	
+	public ValidationException(String message) {
+		super(message);
+		this.messageCode = "";
+		this.messageParams = new HashMap<>();
+	}
+	
+	public ValidationException(String message, Throwable cause) {
+		super(message, cause);
+		this.messageCode = "";
+		this.messageParams = new HashMap<>();
+	}
+	
+	public ValidationException(String message, String messageCode) {
+		super(message);
+		this.messageCode = messageCode;
+		this.messageParams = new HashMap<>();
+	}
+	
+	public ValidationException(String message, String messageCode, Throwable cause) {
+		super(message, cause);
+		this.messageCode = messageCode;
+		this.messageParams = new HashMap<>();
+	}
+	
+	public ValidationException(String message, String messageCode, Map<String, String> messageParams) {
+		super(message);
+		this.messageCode = messageCode;
+		this.messageParams = messageParams;
+	}
+	
+	public ValidationException(String message, String messageCode, Map<String, String> messageParams, Throwable cause) {
+		super(message, cause);
+		this.messageCode = messageCode;
+		this.messageParams = messageParams;
 	}
 	
 }
