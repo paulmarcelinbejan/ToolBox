1273c627163c1d4393b3fa6b5b7b105c0c9ddcc1
Paul Marcelin Bejan <paulmarcelinbejan.developer@gmail.com>
2025-01-30 23:49:43 +0100

    fix: renamed toolbox-web into toolbox-utils-rest

---
+++ toolbox-utils-rest/src/main/java/io/github/paulmarcelinbejan/toolbox/web/response/ResponseStatus.java
@@ -0,0 +1,18 @@
+package io.github.paulmarcelinbejan.toolbox.web.response;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.databind.JsonNode;
+
+@JsonFormat(shape = JsonFormat.Shape.STRING)
+public enum ResponseStatus {
+
+	OK,
+	KO;
+	
+	@JsonCreator
+	public static ResponseStatus fromJsonNode(JsonNode jsonNode) {
+		return ResponseStatus.valueOf(jsonNode.asText());
+	}
+
+}
