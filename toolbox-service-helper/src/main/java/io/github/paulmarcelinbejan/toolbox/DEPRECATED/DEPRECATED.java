9878da0c1b89a3a69aea3b08ece7482863f8d347
Paul Marcelin Bejan <paulmarcelinbejan.developer@gmail.com>
2025-01-30 23:49:02 +0100

    fix: removed deprecated toolbox-service-helper

--- toolbox-service-helper/src/main/java/io/github/paulmarcelinbejan/toolbox/DEPRECATED/DEPRECATED.java
+++
@@ -1,5 +0,0 @@
-package io.github.paulmarcelinbejan.toolbox.DEPRECATED;
-
-public class DEPRECATED {
-
-}
--- toolbox-service-helper/src/main/java/io/github/paulmarcelinbejan/toolbox/service/helper/mapping/MapperUpdateEntity.java
+++
@@ -1,7 +0,0 @@
-package io.github.paulmarcelinbejan.toolbox.service.helper.mapping;
-
-public interface MapperUpdateEntity<ENTITY> {
-	
-	void updateEntity(ENTITY toUpdate, ENTITY newValue);
-
-}
